0fa8e4162628ab34b4d75d4d7ab8889ac7f2e72b
Yevhenii Kononets <yudzhin1408@gmail.com>
2022-03-26 20:27:27 +0200

    Объекты

--- styles-3.js
+++ styles-3.js
@@ -1,24 +1,24 @@
-const btnAdd = document.querySelector('button[data-add]');
-const resetBtn = document.querySelector('button[data-reset]');
-const valueInput = document.querySelector('input[data-value]');
-const outrotEl=document.querySelector('.js-output span')
+// const btnAdd = document.querySelector('button[data-add]');
+// const resetBtn = document.querySelector('button[data-reset]');
+// const valueInput = document.querySelector('input[data-value]');
+// const outrotEl=document.querySelector('.js-output span')
 
-let total = 0;
+// let total = 0;
 
-btnAdd.addEventListener('click', function() {
-    console.log('click heheheheheh');
+// btnAdd.addEventListener('click', function() {
+//     console.log('click heheheheheh');
 
-    const value = Number(valueInput.value);
+//     const value = Number(valueInput.value);
 
     
 
-    total += value;
-    outrotEl.textContent = total;
+//     total += value;
+//     outrotEl.textContent = total;
     
-    valueInput.value = '';
-})
+//     valueInput.value = '';
+// })
 
-resetBtn.addEventListener('click', function () {
-    outrotEl.textContent = 0;
-})
+// resetBtn.addEventListener('click', function () {
+//     outrotEl.textContent = 0;
+// })
 
