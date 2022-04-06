c3a80be0b2ad92faf5d4ec016bd0b18e898ec278
Yevhenii Kononets <yudzhin1408@gmail.com>
2022-04-06 21:55:55 +0300

    Update lesson-4_2.js

--- js-Example-Repeta/lesson-4_2.js
+++ js-Example-Repeta/lesson-4_2.js
@@ -331,7 +331,7 @@
 // console.log(evenNumbers);
 // console.log(oddNumbers);
 // =================================================================
-// const users = 
+// const users =
 //   [
 //     {
 //       name: "Moore Hensley",
@@ -411,5 +411,169 @@
 // console.log(getTotalBalanceByGender(users, 'male'));
 // console.log(getTotalBalanceByGender(users, 'female'));
 // =================================================================
+/*
+ * Array.prototype.sort(callback(currentEl, nextEl){})
+ * - Сортирует и ИЗМЕНЯЕТ оригинальный массив
+ * - По умолчанию:
+ *    - сортирует по возрастанию
+ *    - приводит элементы к строке и сортирует по [Unicode](https://unicode-table.com/en/)
+ */
+
+// const numbers = [1, 9, 6, 2, 3];
+// numbers.sort();
+// console.log('numbers', numbers);
+
+// const letters = ['b', 'B', 'a', 'A'];
+// letters.sort();
+// console.log('letters', letters);
+
+/*
+ * compareFunction - функция сравнения (callback)
+ * Элементы массива сортируются в соответствии с её возвращаемым значением
+ *  - eсли compareFunction(A, B) меньше 0, сортировка поставит A перед B
+ *  - если compareFunction(A, B) больше 0, сортировка поставит B перед A
+ *  - если compareFunction(A, B) вернёт 0, сортировка оставит A и B на неизменными по отношению друг к другу, но отсортирует их по отношению ко всем другим элементам.
+ */
+
+// numbers.sort((curEl, nextEl) => {
+//     return nextEl - curEl;
+// });
+
+/*
+ * Как сделать копию массива чтобы не сортировать оригинальный
+ * - Array.prototype.slice()
+ * - Операция spread
+ */
+
+// const descSortedNumbers = [...numbers].sort((a, b) => b - a);
+// const ascSortedNumbers = [...numbers].sort((a, b) => a - b);
+// console.log('descSortedNumbers', descSortedNumbers);
+// console.log('ascSortedNumbers', ascSortedNumbers);
+
+/*
+ * Кастомная сортировка сложных типов
+ */
+// const players = [
+//     { id: 'player-1', name: 'Mango', timePlayed: 310, online: false },
+//     { id: 'player-2', name: 'Poly', timePlayed: 470, online: true },
+//     { id: 'player-3', name: 'Aiwi', timePlayed: 230, online: true },
+//     { id: 'player-4', name: 'Ajax', timePlayed: 150, online: false },
+//     { id: 'player-5', name: 'Chelsey', timePlayed: 80, online: true },
+// ];
+
+// По игровому времени
+// const sortedByBestPlayers = [...players].sort(
+//     (prevPlayer, nextPlayer) => nextPlayer.timePlayed - prevPlayer.timePlayed,
+// );
+// console.table(sortedByBestPlayers);
+
+// const sortedByWorstPlayers = [...players].sort(
+//     (prevPlayer, nextPlayer) => prevPlayer.timePlayed - nextPlayer.timePlayed,
+// );
+// console.table(sortedByWorstPlayers);
+
+// const byName = [...players].sort((a, b) => {
+//     const result = a.name[0] > b.name[0];
+
+//     if (result) {
+//         return 1;
+//     }
+
+//     if (!result) {
+//         return -1;
+//     }
+// });
+
+// console.table(byName);
+
+// ========================================================================
+
+/*
+ * Array.prototype.flat(depth)
+ * - Разглаживает массив до указанной глубины
+ * - По умолчанию глубина 1
+ */
+
+// const array = [1, 2, [4, [5]], [6, [7, 8, [9]]]];
+
+// console.log(array.flat(3));
 
 
+/*
+ * Array.prototype.flatMap(callback)
+ * - Комбинация map + flat
+ */
+
+// const tweets = [
+//     { id: '000', likes: 5, tags: ['js', 'nodejs'] },
+//     { id: '001', likes: 2, tags: ['html', 'css'] },
+//     { id: '002', likes: 17, tags: ['html', 'js', 'nodejs'] },
+//     { id: '003', likes: 8, tags: ['css', 'react'] },
+//     { id: '004', likes: 0, tags: ['js', 'nodejs', 'react'] },
+// ];
+
+// const tags = tweets.flatMap(t => t.tags);
+// console.log(tags);
+
+// ==================================================================
+/*
+ * Цепочки вызовов - chaining
+ */
+// const numbers = [1, 5, 2, 4, 3];
+// const greaterThenTwo = numbers.filter(num => num > 2);
+// console.log(greaterThenTwo);
+
+// const multByThree = greaterThenTwo.map(num => num * 3);
+// console.log(multByThree);
+
+// const sorted = multByThree.sort((a, b) => a - b);
+// console.log(sorted);'
+
+// Цепочка предыдущих трёх
+// const sorted = numbers.filter(num => num > 2)
+//     .map(num => num * 3)
+//     .sort((a, b)=> a - b);
+
+//     console.log(sorted);
+// ==================================================================
+
+/*
+ * Сортируем тех кто онлайн по рангу
+ * - сначала фильтруем
+ * - потом сортируем
+ */
+// const players = [
+//     { id: 'id-1', tag: 'Mango', isOnline: true, rank: 800 },
+//     { id: 'id-2', tag: 'Poly', isOnline: false, rank: 600 },
+//     { id: 'id-3', tag: 'Ajax', isOnline: true, rank: 100 },
+//     { id: 'id-4', tag: 'Kiwi', isOnline: true, rank: 400 },
+//     { id: 'id-5', tag: 'Chelsy', isOnline: false, rank: 200 },
+// ];
+
+// const onlineAndSorted = players
+//     .filter(players => players.isOnline)
+//     .sort((playerA, playerB) => playerA.rank - playerB.rank);
+// console.table(onlineAndSorted);
+// ==================================================================
+
+/*
+ * Chaining в методах объекта как jquery
+ */
+
+// const element = {
+//     class: '',
+//     hovered: false,
+//     changeClass(cls) {
+//         this.class = cls;
+//         return this;
+//     },
+//     toggleHovered() {
+//         this.hovered = !this.hovered;
+//         return this;
+//     },
+// };
+
+// element.toggleHovered().changeClass('open').toggleHovered();
+
+// console.log(element);
+// ==================================================================
