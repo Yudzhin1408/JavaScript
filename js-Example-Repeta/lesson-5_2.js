aab2b13e41725fdd4212b6cefe670f6f216e07b2
Yevhenii Kononets <yudzhin1408@gmail.com>
2022-04-10 01:25:54 +0300

    Update lesson-5_2.js

--- js-Example-Repeta/lesson-5_2.js
+++ js-Example-Repeta/lesson-5_2.js
@@ -46,3 +46,274 @@
 
 // console.log(objA.x);
 // =======================================================
+ /*
+ * Основы ООП: класс, экземпляр (объект), интерфейс
+ */
+
+/*
+ * Функции-конструкторы
+ * - Именование
+ * - Оператор new
+ * - Свойство Function.prototype
+ */
+// const Car = function ({ brand, model, price } = {}) {
+//       // const { brand, model, price } = config;
+//   // 2. Функция вызывается в контексте созданного объекта,
+//   //    то есть в this записывается ссылка на него
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//       // 3. В свойство this.__proto__ записывается ссылка на обьект Car.prototype
+//   //    тоесть Car.prototype это ПРОТОТИП будущего обьекта (экземпляра)
+
+//   // 4. Ссылка на обьект возвращается в место вызова new Car
+// };
+
+// Car.prototype.sayHi = function () {
+//     console.log('Car.prototype.sayHi->this', this);
+//     console.log('Hello :)');
+// };
+
+
+// Car.prototype.changePrice = function (newPrice) {
+//     this.price = newPrice;
+// };
+// //  1. Если функция вызывается через new, создаётся пустой объект
+// const myCar = new Car({
+    
+//     brand: 'Audi',
+//     model: 'Q3',
+//     price: 35000,
+// });
+// console.log(myCar);
+
+// myCar.sayHi();
+// myCar.changePrice(10000);
+
+// const myCar2 = new Car({
+    
+//     brand: 'BMW',
+//     model: 'X6',
+//     price: 50000,
+// });
+// console.log(myCar2);
+
+// const myCar3 = new Car({
+    
+//     brand: 'Audi',
+//     model: 'A6',
+//     price: 65000,
+// });
+// console.log(myCar3);
+// myCar3.changePrice(70000);
+// console.log('new price', myCar3);
+
+// =======================================================
+
+// const User = function ({ email, password } = {}) {
+//     this.email = email;
+//     this.password = password;
+// };
+// console.log(User.prototype);
+
+// User.prototype.changeEmail = function (newMail) {
+//     this.email = newMail;
+// };
+
+// const mango = new User({ email: 'mango@mail.com', password: 111111 });
+
+// console.log(mango);
+
+// mango.changeEmail('my-new-mail@mail.com');
+// console.log(mango);
+
+
+// =======================================================
+
+
+/*
+ * Статические свойства и методы
+ * - Статические свойства и методы доступны только на самом конструкторе
+ * - В статических методах не нужен this
+ */
+
+// User.message ='Я статическое свойство, меня нет на экземплярах или в прототипе.';
+// User.logInfo = function (obj) {
+//     console.log('User.logInfo->obj', obj);
+//     console.log('Почта:', obj.email);
+//     console.log('Пароль:', obj.password);
+    
+// };
+// User.logInfo(mango);
+
+
+
+
+// 1. У каждого обьекта есть свойство __proto__
+// 2. В этом свойстве лежит ссылка на его ПРОТОТИП, то есть другой обьект
+// 3. При создании литера обьекта, в свойство __proto__ записывается ссылка на Функция.prototype
+// 4. Функция-конструктор это просто функция :)
+// 5. Всю магию делает оператор new
+// 6. Если функция вызывается через new, создаётся пустой объект
+// 7. Функция вызывается в контексте созданного объекта
+// 8. В свойство this.__proto__ записывается ссылка на обьект Функция.prototype
+// 9. Ссылка на обьект возвращается в место вызова new Фунукция()
+
+// ======================================================
+
+/*
+ * Классы
+ * 🐷 - объявление
+ * 🐷 - конструктор
+ * 🐷 - методы
+ * 🐷 - static
+ * 🐷 - приватные свойства
+ * 🐷 - синтаксис публичных свойства и методы классов
+ * 🐷 - геттеры и сеттеры
+ */
+
+ 
+
+// class Car {
+//     static description = 'Класс описывающий автомобиль';
+
+//     static logInfo(carObj) {
+//         console.log('Car.logInfo-> carObj', carObj);
+//     }
+//     constructor({ brand, model, price } = {}) {
+//         this.brand = brand;
+//         this._model = model;
+//         this._price = price;
+
+//     }
+//     get price(){
+//         return this._price;
+//     }
+
+//     set price(newPrice) {
+//         this._price = newPrice;
+//     }
+
+//       get model(){
+//         return this._model;
+//     }
+
+//     set model(newModel) {
+//         this._model = newModel;
+//     }
+
+// };
+// const carInstance = new Car({
+//     brand: 'Audi',
+//     model: 'Q3',
+//     price: 35000,
+// });
+
+
+// console.log(carInstance);
+
+
+// console.log(carInstance.model);
+// carInstance.model = 'Q4';
+// console.log(carInstance.model);
+
+
+// console.log(carInstance.price);
+// carInstance.price = 50000;
+// console.log(carInstance.price);
+
+// console.log(carInstance);
+
+// ======================================================
+
+/*
+ * Наследование
+ *  - extends
+ *  - super()
+ */
+
+
+class Hero{ 
+    constructor({ name = 'hero', xp = 0 } = {}){
+    this.name = name;
+    this.xp = xp;
+    }
+
+    gainXp(amount) {
+        console.log(`${this.name} получает ${amount} опыта`);
+        this.xp += amount;
+    }
+}
+
+class Warrior extends Hero {
+    constructor({ weapon, ...restProps } = {}) {
+        super(restProps);
+        this.weapon = weapon;
+    }
+
+    attack() {
+        console.log(`${this.name} атакует используя ${this.weapon}`);
+    
+    }
+}
+ 
+class Berserk extends Warrior{
+    constructor({ warcry, ...restProps } = {}) {
+        super(restProps);
+
+        this.warcry = warcry;
+    }
+
+    babyRege() {
+        console.log(this.warcry);
+    }
+}
+
+const ajax = new Berserk({
+    name: 'ajax',
+    xp: 500,
+    weapon: 'axe',
+    warcry: 'waaaaaaaaaah',
+});
+
+
+console.log(ajax);
+
+ajax.babyRege();
+ajax.attack();
+ajax.gainXp();
+
+
+class Mage extends Hero {
+    constructor({ spells, ...restProps } = {}) {
+        super(restProps);
+
+        this.spells = spells;
+    }
+
+    cast() {
+console.log(`${this.name} что-то там кастует 🧙‍♂️`);
+        
+    }
+}
+
+ const mango = new Warrior({ name: 'mango', xp: 1000, weapon: 'алебарда' });
+console.log(mango);
+mango.attack();
+mango.gainXp(1000);
+
+
+const poly = new Mage({ name: 'poly', xp: 500, spells: ['фаербол'] });
+
+console.log(
+    'Warrior.prototype.__proto__ === Hero.prototype ',
+    Warrior.prototype.__proto__ === Hero.prototype,
+);
+
+console.log(
+    'Hero.prototype.__proto__ === Object.prototype ',
+    Hero.prototype.__proto__ === Object.prototype,
+);
+
+
+console.log(poly);
